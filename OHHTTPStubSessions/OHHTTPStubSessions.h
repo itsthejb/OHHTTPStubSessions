e53745aa0ed78c690ce9e31ede2c76f2ff5fa453
Jonathan Crooke <joncrooke@gmail.com>
2014-02-24 10:49:04 +0100

    Initial

---
+++ OHHTTPStubSessions/OHHTTPStubSessions.h
@@ -0,0 +1,13 @@
+//
+//  OHHTTPStubSessions.h
+//  OHHTTPStubSessions
+//
+//  Created by Jonathan Crooke on 24/02/2014.
+//  Copyright (c) 2014 Jonathan Crooke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OHHTTPStubSessions : NSObject
+
+@end
---
+++ OHHTTPStubSessions/OHHTTPStubSessions.m
@@ -0,0 +1,13 @@
+//
+//  OHHTTPStubSessions.m
+//  OHHTTPStubSessions
+//
+//  Created by Jonathan Crooke on 24/02/2014.
+//  Copyright (c) 2014 Jonathan Crooke. All rights reserved.
+//
+
+#import "OHHTTPStubSessions.h"
+
+@implementation OHHTTPStubSessions
+
+@end
